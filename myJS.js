dc041269dc527162bf3eb09b1742f37107faf3e3
Viktoriia <victoriia9489@gmail.com>
2025-03-15 15:38:12 +0800

    HW

---
+++ myJS.js
@@ -0,0 +1,26 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const modal = document.getElementById("modal");
+    const openBtn = document.getElementById("openModal");
+    const closeBtn = document.getElementById("closeModal");
+
+    openBtn.addEventListener("click", () => {
+        modal.showModal();
+    });
+
+    closeBtn.addEventListener("click", () => {
+        modal.closest();
+    });
+
+    modal.addEventListener("click", (e) => {
+const rect = modal.getBoundingClientRect();
+if (
+    event.clientX < rect.lef ||
+    event.clientX > rect.right ||
+    event.clientY < rect.top ||
+    event.clientY > rect.bottom
+) {
+    modal.closest();
+}
+    });
+
+})
